989e497217ba09e06566304ac71010b5dec6322d
Jay Beavers <jay@hikinghomeschoolers.org>
2015-11-07 08:20:59 -0800

    Move over to es6 syntax, add jshintrc for esnext

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ app.use(logger('dev'));
 app.use(express.static('public'));
 
 /// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use( (req, res, next) => {
   var err = new Error('Not Found: ' + req.url);
   err.status = 404;
   next(err);
 });
 
 /// error handlers
-app.use(function(err, req, res, next) {
+app.use( (err, req, res, next) => {
   res.writeHead(err.status || 500);
   res.end();
 });
