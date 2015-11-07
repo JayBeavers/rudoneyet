ae784a0120a41a067495f931179dab77fee5e22e
Jay Beavers <jay@hikinghomeschoolers.org>
2015-11-07 07:57:52 -0800

    Initial checkin of express website skeleton

---
+++ app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var logger = require('morgan');
+
+var app = express();
+app.use(logger('dev'));
+
+app.use(express.static('public'));
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found: ' + req.url);
+  err.status = 404;
+  next(err);
+});
+
+/// error handlers
+app.use(function(err, req, res, next) {
+  res.writeHead(err.status || 500);
+  res.end();
+});
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict';
+
+var debugService = require('debug');
+debugService.enable('server');
+
+var debug = require('debug')('server');
+
+var app = require('./app');
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function() {
+    debug('Express server listening on port ' + server.address().port);
+});
