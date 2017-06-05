1fd4e41cbcef039ff9427f0f941a6e2987b75882
Jay Beavers <jay@hikinghomeschoolers.org>
2017-06-04 20:00:22 -0700

    Add docker support

---
+++ admin/run.sh
@@ -0,0 +1,6 @@
+eval $(docker-machine env hikinghomeschoolers)
+docker build -t jaybeavers/rudoneyet ..
+docker stop rudoneyet
+docker rm rudoneyet
+docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
+docker run -d --name rudoneyet -e TRELLO_KEY -e TRELLO_SECRET -e "VIRTUAL_HOST=www.rudoneyet.com" -e "LETSENCRYPT_HOST=www.rudoneyet.com" -e "LETSENCRYPT_EMAIL=jay@hikinghomeschoolers.org" jaybeavers/rudoneyet
