278f54d3bbfdf5a99e21e233f5a77333a9adec51
Jay Beavers <jay@hikinghomeschoolers.org>
2017-06-06 06:49:12 -0700

    Support naked domain as well as www

--- admin/run.sh
+++ admin/run.sh
@@ -3,4 +3,4 @@ docker build -t jaybeavers/rudoneyet ..
 docker stop rudoneyet
 docker rm rudoneyet
 docker rmi `docker images --filter 'dangling=true' -q --no-trunc`
-docker run -d --name rudoneyet -e TRELLO_KEY -e TRELLO_SECRET -e "VIRTUAL_HOST=www.rudoneyet.com" -e "LETSENCRYPT_HOST=www.rudoneyet.com" -e "LETSENCRYPT_EMAIL=jay@hikinghomeschoolers.org" jaybeavers/rudoneyet
+docker run -d --name rudoneyet -e TRELLO_KEY -e TRELLO_SECRET -e "VIRTUAL_HOST=www.rudoneyet.com,rudoneyet.com" -e "LETSENCRYPT_HOST=www.rudoneyet.com,rudoneyet.com" -e "LETSENCRYPT_EMAIL=jay@hikinghomeschoolers.org" jaybeavers/rudoneyet
